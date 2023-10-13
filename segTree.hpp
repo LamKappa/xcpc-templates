46a6c58de9a520d1a0f36258fc69664232d1feb7
LamKappa <xc17725067706@163.com>
2023-10-13 09:23:57 +0800

    231013 update SegTree bound & node-info

--- segTree.hpp
+++ segTree.hpp
@@ -1,6 +1,9 @@
 #ifndef SEGTREE_H
 #define SEGTREE_H
 // 线段树
+// 动态开点
+// 值域+权值
+// 区间delayTag
 
 struct Tag{
     int mul = 1;
@@ -31,33 +34,36 @@ struct Info{
 
 template<class Info, class Tag>
 struct SegTree{
-    int n, l_bound, r_bound;
+    int l_bound, r_bound;
     struct Node{
         Info info;
         Tag tag;
+        int l, r;
         int chl=-1, chr=-1;
+        Node(int l, int r){
+            this->l = l;
+            this->r = r;
+        }
     };
     std::vector<Node> node;
 
     SegTree(){}
-    SegTree(int n):SegTree(){init(n,0,n);}
-    SegTree(int l_bound, int r_bound):SegTree(){init(r_bound-l_bound,l_bound,r_bound);}
+    SegTree(int n):SegTree(){init(0,n);}
+    SegTree(int l_bound, int r_bound):SegTree(){init(l_bound,r_bound);}
     SegTree(int n, Info v):SegTree(){init(std::vector(n,v));}
     SegTree(const std::vector<Info>&initarr):SegTree(){init(initarr);}
-    void init(int n, int l_bound, int r_bound){
-        this->n = n;
+    void init(int l_bound, int r_bound){
         this->l_bound = l_bound;
         this->r_bound = r_bound;
-        node.emplace_back();
+        node.emplace_back(l_bound, r_bound);
     }
     void init(const std::vector<Info>&initarr){
-        init(initarr.size(),0,initarr.size());
-        node.reserve(4<<std::__lg(n));
+        init(0,initarr.size());
+        node.reserve(4<<std::__lg(r_bound - l_bound));
         std::function<void(int,int,int)> build = [&](int rt,int l,int r){
             if(l+1==r) return (void)(node[rt].info = initarr[l]);
-            int mid = (l+r)/2;
             __push(rt);
-            build(node[rt].chl, l, mid); build(node[rt].chr, mid, r);
+            build(node[rt].chl, l, (l+r)/2); build(node[rt].chr, (l+r)/2, r);
             __pull(rt);
         };
         build(0, l_bound, r_bound);
@@ -68,8 +74,8 @@ struct SegTree{
         node[rt].tag += v;
     }
     void __push(int rt){
-        if(node[rt].chl<0) node[rt].chl = node.size(), node.emplace_back();
-        if(node[rt].chr<0) node[rt].chr = node.size(), node.emplace_back();
+        if(node[rt].chl<0) node[rt].chl = node.size(), node.emplace_back(node[rt].l, (node[rt].l+node[rt].r)/2);
+        if(node[rt].chr<0) node[rt].chr = node.size(), node.emplace_back((node[rt].l+node[rt].r)/2, node[rt].r);
         __apply(node[rt].chl, node[rt].tag);
         __apply(node[rt].chr, node[rt].tag);
         node[rt].tag = Tag();
@@ -80,35 +86,33 @@ struct SegTree{
     template<typename Modifier>
     void __modify(int rt, int l, int r, int L, int R, const Modifier&func){
         if(R<=l || r<=L) return;
-        if(L<=l && r<=R) return (void)(func(rt,l,r));
-        int mid = (l+r)/2;
+        if(L<=l && r<=R) return (void)(func(rt));
         __push(rt);
-        if(L<mid) __modify(node[rt].chl, l, mid, L, R, func);
-        if(R>mid) __modify(node[rt].chr, mid, r, L, R, func);
+        if(L<(l+r)/2) __modify(node[rt].chl, l, (l+r)/2, L, R, func);
+        if(R>(l+r)/2) __modify(node[rt].chr, (l+r)/2, r, L, R, func);
         __pull(rt);
     }
     void assign(int L, int R, const Info&v){
-        __modify(0, l_bound, r_bound, L, R+1, [&](int x,int l,int r){
-            node[x].info = v;
+        __modify(0, l_bound, r_bound, L, R+1, [&](int rt){
+            node[rt].info = v;
         });
     }
     void mul(int L, int R, int v){
-        __modify(0, l_bound, r_bound, L, R+1, [&](int x,int l,int r){
-            __apply(x,Tag{v,0});
+        __modify(0, l_bound, r_bound, L, R+1, [&](int rt){
+            __apply(rt,Tag{v,0});
         });
     }
     void add(int L, int R, int v){
-        __modify(0, l_bound, r_bound, L, R+1, [&](int x,int l,int r){
-            __apply(x,Tag{1,v});
+        __modify(0, l_bound, r_bound, L, R+1, [&](int rt){
+            __apply(rt,Tag{1,v});
         });
     }
 
     Info __ask(int rt, int l, int r, int L, int R){
         if(R<=l || r<=L) return Info();
         if(L<=l && r<=R) return node[rt].info;
-        int mid = (l+r)/2;
         __push(rt);
-        return __ask(node[rt].chl, l, mid, L, R) + __ask(node[rt].chr, mid, r, L, R);
+        return __ask(node[rt].chl, l, (l+r)/2, L, R) + __ask(node[rt].chr, (l+r)/2, r, L, R);
     }
     Info ask(int L, int R){
         return __ask(0, l_bound, r_bound, L, R+1);
@@ -117,13 +121,14 @@ struct SegTree{
     static const int npos = -1;
     template<class Predicator>
     int __find(int rt, int l, int r, int L, int R, const Predicator&pred, bool forward){
-        if(R<=l || r<=L || !pred(node[rt])) return npos;
+        if(R<=l || r<=L || !pred(node[rt].info)) return npos;
         if(l+1==r) return l;
-        int mid = (l+r)/2;
         __push(rt);
-        int pos = __find(forward?node[rt].chl:node[rt].chr, l, mid, L, R, pred, forward);
+        int chl = node[rt].chl, chr = node[rt].chr;
+        if(forward) std::swap(chl, chr);
+        int pos = __find(chl, l, (l+r)/2, L, R, pred, forward);
         if(pos == npos){
-            pos = __find(forward?node[rt].chr:node[rt].chl, mid, r, L, R, pred, forward);
+            pos = __find(chr, (l+r)/2, r, L, R, pred, forward);
         }
         return pos;
     }
