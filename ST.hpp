e2fee13ad810aafa93c13340f568798e0467c2a4
LamKappa <xc17725067706@163.com>
2023-10-24 09:27:16 +0800

    231024 update ST.hpp

---
+++ ST.hpp
@@ -0,0 +1,85 @@
+#ifndef ST_H
+#define ST_H
+
+template<class T>
+struct ST{
+    unsigned N, B;
+    std::function<T(T,T)> calc;
+    std::vector<std::vector<T>> a, b;
+    std::vector<T> pre, suf;
+
+    ST(){}
+    ST(const std::vector<T>&v, 
+        const std::function<T(T,T)>&calc = [](T x,T y){return std::min(x, y);}){
+        init(v, calc);
+    }
+    template<typename ... Args>
+    T Calc(const T&x, const T&y, Args ... args){
+        return Calc(calc(x, y), args...);
+    }
+    T Calc(const T&x, const T&y){return calc(x, y);}
+    void init(const std::vector<T>&v, 
+        const std::function<T(T,T)>&calc = [](T x,T y){return std::min(x, y);}){
+        this->N = v.size();
+        if(N <= 0) return;
+        this->B = sqrt(N) + 1;
+        this->calc = calc;
+        pre = suf = v;
+        const int M = (N + B - 1) / B;
+        const int lgM = std::__lg(M);
+        const int lgB = std::__lg(B);
+        a.resize(lgM+1); a[0].resize(M);
+        b.resize(lgB+1); b[0] = v;
+        for(int i=0; i<M; i++){
+            a[0][i] = v[i * B];
+            for(int j=1; j<B && i*B + j < N; j++){
+                a[0][i] = Calc(a[0][i], v[i*B + j]);
+            }
+        }
+        for(int j=0; j<lgM; j++){
+            a[j+1].resize(M - (2<<j) + 1);
+            for(int i=0; i+(2<<j)<=M; i++){
+                a[j+1][i] = Calc(a[j][i], a[j][i + (1<<j)]);
+            }
+        }
+        for(int j=0; j<lgB && (2<<j)<=N; j++){
+            b[j+1].resize(N - (2<<j) + 1);
+            for(int i=0; i+(2<<j)<=N; i++){
+                b[j+1][i] = b[j][i];
+                if((i+(1<<j))/B == i/B){
+                    b[j+1][i] = Calc(b[j+1][i], b[j][i + (1<<j)]);
+                }
+            }
+        }
+        for(int i=1; i<N; i++){
+            if(i%B != 0){
+                pre[i] = Calc(pre[i], pre[i-1]);
+            }
+        }
+        for(int i=N-2; i>=0; i--){
+            if(i%B != B-1){
+                suf[i] = Calc(suf[i], suf[i+1]);
+            }
+        }
+    } 
+    T ask(int l, int r){
+        if(l > r) std::swap(l, r);
+        assert(0<=l && r < N);
+        if(l/B != r/B){
+            T ans = Calc(suf[l], pre[r]);
+            l = l/B + 1;
+            r = r/B - 1;
+            if(l <= r){
+                int k = std::__lg(r-l+1);
+                ans = Calc(ans, a[k][l], a[k][r + 1 - (1<<k)]);
+            }
+            return ans;
+        } else{
+            int k = std::__lg(r-l+1);
+            return Calc(b[k][l], b[k][r + 1 - (1<<k)]);
+        }
+    }
+};
+
+
+#endif
--- bit.hpp
+++ bit.hpp
@@ -14,7 +14,7 @@ struct BIT{
     }
     void init(const std::vector<T>&initarr){
         init(initarr.size());
-        for(int i=1;i<=n;i++){
+        for(int i=1; i<=n; i++){
             node[i] += initarr[i-1];
             if(i+lowbit(i)<=n)node[i+lowbit(i)] += node[i];
         }
@@ -23,7 +23,7 @@ struct BIT{
         this->n = n;
         node.assign(n+1,T());
     }
-    void add(int k,T v){
+    void add(int k, T v){
         if(!k)return;
         for(;k<=n;k+=lowbit(k))node[k]+=v;
     }
@@ -32,15 +32,15 @@ struct BIT{
         for(;k>0;k-=lowbit(k))ret+=node[k];
         return ret;
     }
-    T ask(int l,int r){
+    T ask(int l, int r){
         if(l>r)std::swap(l,r);
         return query(r) - query(l-1);
     }
     T ask(int p){return ask(p,p);}
     int kth(T k){
         int x = 0;
-        for(int i=1<<std::__lg(n);i;i>>=2){
-            if(x+i<=n&&k>=node[x+i-1]){
+        for(int i=1<<std::__lg(n); i; i>>=2){
+            if(x+i<=n && k>=node[x+i-1]){
                 x += i;
                 k -= node[x-1];
             }
@@ -59,33 +59,33 @@ struct BIT_range{
     }
     void init(const std::vector<T>&initarr){
         init(initarr.size());
-        for(int i=1;i<=n;i++){
+        for(int i=1; i<=n; i++){
             di.node[i] += i*initarr[i-1];
-            if(i<n)di.node[i+1] -= (i+1)*initarr[i-1];
-            if(i+lowbit(i)<=n)di.node[i+lowbit(i)] += di.node[i];
+            if(i<n) di.node[i+1] -= (i+1)*initarr[i-1];
+            if(i+lowbit(i)<=n) di.node[i+lowbit(i)] += di.node[i];
             dn.node[i] += initarr[i-1];
-            if(i<n)dn.node[i+1] -= initarr[i-1];
-            if(i+lowbit(i)<=n)dn.node[i+lowbit(i)] += dn.node[i];
+            if(i<n) dn.node[i+1] -= initarr[i-1];
+            if(i+lowbit(i)<=n) dn.node[i+lowbit(i)] += dn.node[i];
         }
     }
     void init(int n){
         this->n = n;
-        di.init(n);dn.init(n);
+        di.init(n); dn.init(n);
     }
-    void __add(int k,int v){
+    void __add(int k, int v){
         di.add(k,k*v);
         dn.add(k,v);
     }
-    void add(int l,int r,T v){
-        if(l>r)std::swap(l,r);
-        __add(l,v);__add(r+1,-v);
+    void add(int l, int r, T v){
+        if(l>r) std::swap(l,r);
+        __add(l,v); __add(r+1,-v);
     }
     void add(int k,T v){add(k,k,v);}
     T ask(int k){
         return (k+1)*dn.ask(k) - di.ask(k);
     }
-    T ask(int l,int r){
-        if(l>r)std::swap(l,r);
+    T ask(int l, int r){
+        if(l>r) std::swap(l,r);
         return ask(r) - ask(l-1);
     }
 };
