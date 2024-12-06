77549e08c831cee170e9680ae539b45492584054
LamKappa <xc17725067706@163.com>
2024-12-06 20:49:56 +0800

    use List to mantain lower convex shell

--- Delaunay.hpp
+++ Delaunay.hpp
@@ -19,18 +19,19 @@ namespace Delaunay{
     }
 
     struct Graph{
+        template<typename T>
+        using List = std::list<T>;
         struct Edge{
             int tar;
-            std::list<Edge>::iterator rev;
+            List<Edge>::iterator rev;
         };
         std::vector<int> id;
-        std::vector<std::list<Edge>> adj;
+        std::vector<List<Edge>> adj;
 
-        auto add_edge(int u, std::list<Edge>::iterator posu, int v, std::list<Edge>::iterator posv){
+        auto add_edge(int u, List<Edge>::iterator posu, int v, List<Edge>::iterator posv){
             posu = adj[u].insert(posu, Edge{v});
             posv = adj[v].insert(posv, Edge{u});
-            posu->rev = posv;
-            posv->rev = posu;
+            posu->rev = posv; posv->rev = posu;
             return std::make_pair(posu, posv);
         }
 
@@ -40,26 +41,32 @@ namespace Delaunay{
             std::sort(id.begin(), id.end(), [&](auto i, auto j){
                 return pts[i] < pts[j];
             });
-            adj.resize(pts.size(), {});
+            adj.resize(pts.size());
 
-            auto rec = [&](auto&&rec, int l, int r){
-                if(l + 1 == r) return;
+            auto rec = [&](auto&&rec, int l, int r)->List<int>{
+                if(l + 1 == r) return {l};
                 if(l + 2 == r){
                     add_edge(id[l], adj[id[l]].begin(), id[l+1], adj[id[l+1]].end());
-                    return;
+                    return {l, l + 1};
                 }
                 int m = (l + r) / 2;
-                rec(rec, l, m); rec(rec, m, r);
+                List<int> I;
+                I.splice(I.end(), rec(rec, l, m));
+                auto it = std::prev(I.end());
+                I.splice(I.end(), rec(rec, m, r));
 
-                std::vector<int> I;
-                for(int i=l; i<r; i++){
-                    while(I.size() > 1 && Line{pts[id[I[I.size() - 2]]], pts[id[I.back()]]}.onLeft(pts[id[i]]) < 0){
-                        I.pop_back();
-                    }
-                    I.push_back(i);
-                }
                 int pl, pr;
-                for(int i=0; i<I.size(); i++) if(I[i] < m && m <= I[i+1]) { pl = id[I[i]]; pr = id[I[i + 1]]; break; }
+                for(it=std::next(it); it!=I.end(); it++){
+                    auto it1 = std::prev(it);
+                    auto it2 = std::prev(it1);
+                    while(it1 != I.end() && it2 != I.end() &&
+                        Line{pts[id[*it2]], pts[id[*it1]]}.onLeft(pts[id[*it]]) < 0){
+                        I.erase(it1);
+                        std::tie(it1, it2) = std::make_pair(it2, std::prev(it2));
+                    }
+                    if(*it1 < m && m <= *it) std::tie(pl, pr) = std::make_pair(id[*it1], id[*it]);
+                    if(*it1 >= m) break;
+                }
                 auto[itl, itr] = add_edge(pl, adj[pl].begin(), pr, adj[pr].end());
 
                 for(;;){
@@ -71,7 +78,6 @@ namespace Delaunay{
                         if(L.onLeft(pts[itl->tar]) <= 0) break;
                         auto nxt = std::next(itl);
                         if(nxt != adj[pl].end() &&
-                            L.onLeft(pts[nxt->tar]) >= 0 &&
                             inside_Circular({pts[pl], pts[pr], pts[itl->tar]}, pts[nxt->tar])){
                             adj[itl->tar].erase(itl->rev);
                             itl = adj[pl].erase(itl);
@@ -85,16 +91,16 @@ namespace Delaunay{
                         if(L.onLeft(pts[itr->tar]) <= 0) break;
                         auto pre = std::prev(itr);
                         if(pre != adj[pr].end() &&
-                            L.onLeft(pts[pre->tar]) >= 0 &&
+                            L.onLeft(pts[pre->tar]) > 0 &&
                             inside_Circular({pts[pl], pts[pr], pts[itr->tar]}, pts[pre->tar])){
                             adj[itr->tar].erase(itr->rev);
-                            itr = adj[pl].erase(itr);
-                            itr = std::prev(itr);
+                            itr = std::prev(adj[pl].erase(itr));
                         }else{
                             npr = itr->tar;
                             break;
                         }
                     }
+
                     if(npl == -1 && npr == -1) break;
                     if(npr == -1 || (npl != -1 && !inside_Circular({pts[pl], pts[pr], pts[npl]}, pts[npr]))){
                         itl = std::next(itl->rev);
@@ -108,6 +114,8 @@ namespace Delaunay{
                         std::tie(itl, itr) = add_edge(pl, itl, pr, itr);
                     }
                 }
+
+                return I;
             };
 
             rec(rec, 0, (int)pts.size());
